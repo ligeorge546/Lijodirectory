472181694cd45492e29ce6d3ca3e0a66b7ab66cc
ligeorge546 <52733056+ligeorge546@users.noreply.github.com>
2020-06-14 18:06:06 +1000

    change in subscription

--- storageacc/Main.tf
+++ storageacc/Main.tf
@@ -1,7 +1,7 @@
 variable "client_id" {}
 variable "client_secret" {}
 provider "azurerm"{
-   version     = "~>1.38.0"
+   #version     = "~>1.38.0"
     client_id   = var.client_id
     client_secret   = var.client_secret
     tenant_id   = "7219b637-1ae4-4711-b5ad-a7a2827a7a9d"
